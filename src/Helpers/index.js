a62175b7e50cb9729baf7cc09ee363fd05fe5e15
icharrd <ricardosv46@gmail.com>
2021-12-10 00:16:05 -0500

    deployment

---
+++ src/Helpers/index.js
@@ -0,0 +1,19 @@
+
+ const generarId = () => {
+    const ramdom = Math.random().toString(36).substr(2)
+    const fecha = Date.now().toString(36)
+    return ramdom + fecha
+}
+
+const formatearFecha = fecha =>{
+    const fechaNueva = new Date(fecha)
+    const opciones ={
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit',
+    }
+
+    return fechaNueva.toLocaleDateString('es-ES',opciones)
+}
+
+export {generarId,formatearFecha}
---
+++ src/components/ListadoGastos.jsx
@@ -0,0 +1,36 @@
+import Gasto from "./Gasto"
+
+const ListadoGastos = ({gastos,setGastoEditar,eliminarGasto,filtro,gastosFiltrados}) => {
+    return (
+        <div className="listado-gastos contenedor">
+            {filtro ? (
+                <>
+                    <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay Gastos en Esta Categoria'}</h2>
+                        {gastosFiltrados.map(gasto=>(
+                            <Gasto
+                                key={gasto.id}
+                                gasto={gasto}
+                                setGastoEditar={setGastoEditar}
+                                eliminarGasto={eliminarGasto}
+                            />
+                            ))}
+                </>       
+                ):(
+                <>
+                    <h2>{gastos.length ? 'Gastos' : 'No hay Gastos Aún'}</h2>
+                        { gastos.map(gasto=>(
+                                <Gasto
+                                    key={gasto.id}
+                                    gasto={gasto}
+                                    setGastoEditar={setGastoEditar}
+                                    eliminarGasto={eliminarGasto}
+                                />
+                                ))}
+                </>
+                )
+            }
+        </div>
+    )
+}
+
+export default ListadoGastos
